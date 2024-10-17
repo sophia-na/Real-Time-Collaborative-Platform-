5774ad651c62aa14288372749ea97598939c49d5
sophia-na <amitazh@aol.com>
2024-10-16 17:03:41 -0700

    new front -react, docker work fine

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
 import { createRoot } from 'react-dom/client';
 import App from '../src/App';
 
-// const container = document.getElementById('root');
-// const root = createRoot(container);
-// root.render(<App />);
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(<App />);
